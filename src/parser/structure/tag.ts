0cc5478d1c140a4f6557b038311a9098162efceb
Alexis Hushbeck <github@imgineme.com>
2021-06-24 08:14:28 -0700

    :label: Use New Parser for Tagged Responses
    Using the new parser for tagged responses. Tagged responses are just status messages for responses, so the parsing is fairly simple.
    
    Test plan:
    yarn jest

---
+++ src/parser/structure/tag.ts
@@ -0,0 +1,29 @@
+import { ILexerToken, TokenTypes } from "../../lexer/types";
+
+const RE_TAG_MATCH = /^A[0-9]+$/i;
+
+export default class Tag {
+	public readonly id: number;
+
+	public static match(
+		tokens: ILexerToken<unknown>[],
+		startingIndex = 0,
+	): null | Tag {
+		const token = tokens[startingIndex];
+
+		if (
+			token &&
+			token.type === TokenTypes.atom &&
+			token.value.match(RE_TAG_MATCH)
+		) {
+			// Because it's an Atom, we know it is a string token
+			return new Tag(token as ILexerToken<string>);
+		}
+
+		return null;
+	}
+
+	constructor(token: ILexerToken<string>) {
+		this.id = parseInt(token.value.substr(1));
+	}
+}
