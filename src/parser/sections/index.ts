f88062c3b79eb31e1060a9b083a0a9eba97f19c6
Alexis Hushbeck <github@imgineme.com>
2021-08-15 11:13:08 -0700

    :wastebasket: Delete Old Parsing Code
    Delete the last pieces of the old parsing code

--- src/parser/sections/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./body";
-export * from "./boxes";
-export * from "./common";
-export * from "./esearch";
-export * from "./fetch";
-export * from "./header";
-export * from "./quota";
